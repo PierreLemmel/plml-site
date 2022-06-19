46425539d420857515df143e2a754e0c2a0465dd
Pierre Lemmel <Pierre.Lemmel@gmail.com>
2022-06-19 12:02:23 +0200

    Add paragraph types in gdoc

--- components/Google/google-doc-displayer.tsx
+++ components/Google/google-doc-displayer.tsx
@@ -1,5 +1,6 @@
-import { docs_v1, google } from 'googleapis';
-import { CSSProperties, Fragment } from 'react';
+import { docs_v1 } from 'googleapis';
+import { type } from 'os';
+import { CSSProperties } from 'react';
 
 export interface GoogleDocDisplayerProps {
     readonly document: docs_v1.Schema$Document;
@@ -9,8 +10,25 @@ const GoogleDocDisplayer = (props: GoogleDocDisplayerProps) => {
 
     const { document } = props;
 
-    const paragraphs = document.body.content
-        .map<Paragraph>(elt => {
+    const blocks = document.body.content
+        .map<BlockContent>(elt => {
+            
+            let blockType: BlockType = 'Paragraph';
+            switch (elt.paragraph?.paragraphStyle?.namedStyleType) {
+                case 'TITLE':
+                    blockType = 'Title';
+                    break;
+                case 'HEADING_1':
+                    blockType = 'Heading1';
+                    break;
+                case 'HEADING_2':
+                    blockType = 'Heading2';
+                    break;
+                case 'HEADING_3':
+                    blockType = 'Heading3';
+                    break;
+            }
+
             return {
                 elements: elt.paragraph?.elements.map(pe => {
 
@@ -23,84 +41,151 @@ const GoogleDocDisplayer = (props: GoogleDocDisplayerProps) => {
                         content,
                         textStyle
                     }
-                })
+                }),
+                blockType
             }
         })
         .filter(elt => elt.elements !== undefined);
 
 
     return <div className='w-full font-mono'>
-        {paragraphs.map((paragraph, i) => <div key={`gdp-${i}`} className="mb-3">
+        {blocks.map((block, i) => {
 
-            {paragraph.elements.map((elt, j) => {
-                const {
-                    content,
-                    textStyle
-                } = elt;
-            
-                const key = `gdpe-${j}`;
+            const key = `gdp-${i}`;
+            const { blockType, elements } = block;
 
-                const { bold, italic, underline, fontSize } = textStyle;
-                const classes = [];
-                const style: CSSProperties = {};
+            switch(blockType) {
+                case 'Paragraph': 
+                    return <ParagraphBlock elements={elements} key={key} />
+                case 'Heading1':
+                    return <Heading1Block elements={elements} key={key} />
+                case 'Heading2':
+                    return <Heading2Block elements={elements} key={key} />
+                case 'Heading3':
+                    return <Heading3Block elements={elements} key={key} />
+                case 'Title':
+                    return <TitleBlock elements={elements} key={key} />
+            }
 
-                if (bold) {
-                    classes.push("font-bold");
-                }
-
-                if (italic) {
-                    classes.push("italic");
-                }
-
-                if (underline) {
-                    classes.push("underline");
-                }
-
-                if (fontSize) {
-                    const fsize = 16 + (fontSize.magnitude - 11);
-                    style.fontSize = fsize + 'px';
-                }
-
-                return <span key={key} style={style} className={classes.join(" ")}>{content}</span>
-            })}
-        </div>)}
+        })}
     </div>;
 }
 
-
-interface Paragraph {
-    readonly elements: ParagraphElement[];
-}
-
-interface ParagraphElement {
-    readonly content: string;
-    readonly textStyle: docs_v1.Schema$TextStyle;
+interface BlockProps {
+    readonly elements: TextElement[];
 }
 
 
-interface GoogleDocParagraphProps {
-    readonly paragraph: Paragraph;
+const titleBaseFontSize = 60;
+const heading1BaseFontSize = 36;
+const heading2BaseFontSize = 24;
+const heading3BaseFontSize = 20;
+const paragraphBaseFontSize = 16;
+
+
+const titleGDocFontSize = 26;
+const heading1GDocFontSize = 20;
+const heading2GDocFontSize = 16;
+const heading3GDocFontSize = 14;
+const paragraphGDocFontSize = 11;
+
+
+const ParagraphBlock = ({ elements }: BlockProps) => <div className={`mb-2`}>
+
+    {elements.map((elt, j) => <BlockTextElement
+        key={`gdpe-${j}`} textElement={elt}
+        gDocBaseSize={paragraphGDocFontSize} baseSize={paragraphBaseFontSize}
+    />)}
+</div>
+
+
+const TitleBlock = ({ elements }: BlockProps) => <div className="text-6xl">
+    {elements.map((elt, j) => <BlockTextElement
+        key={`gdpe-${j}`} textElement={elt}
+        gDocBaseSize={titleGDocFontSize} baseSize={titleBaseFontSize}
+    />)}
+</div>
+
+
+const Heading1Block = ({ elements }: BlockProps) => <div className="text-4xl">
+    {elements.map((elt, j) => <BlockTextElement
+        key={`gdpe-${j}`} textElement={elt}
+        gDocBaseSize={heading1GDocFontSize} baseSize={heading1BaseFontSize}
+    />)}
+</div>
+
+
+const Heading2Block = ({ elements }: BlockProps) => <div className="text-2xl">
+    {elements.map((elt, j) => <BlockTextElement
+        key={`gdpe-${j}`} textElement={elt}
+        gDocBaseSize={heading2GDocFontSize} baseSize={heading2BaseFontSize}
+    />)}
+</div>
+
+
+const Heading3Block = ({ elements }: BlockProps) => <div className="text-xl">
+    {elements.map((elt, j) => <BlockTextElement
+        key={`gdpe-${j}`} textElement={elt}
+        gDocBaseSize={heading3GDocFontSize} baseSize={heading3BaseFontSize}
+    />)}
+</div>
+
+
+interface BlockTextElementProps {
+    readonly textElement: TextElement;
+    readonly gDocBaseSize: number;
+    readonly baseSize: number;
 }
 
-const GoogleDocParagraph = (props: GoogleDocParagraphProps) => {
-    const { paragraph } = props;
-    
-    return <div>
-        {paragraph.elements.map((elt, i) => <GoogleDocParagraphElement key={`gdpe-${i}`} element={elt} />)}
-    </div>
-}
-
-interface GoogleDocParagraphElementProps {
-    readonly element: ParagraphElement;
-}
-
-const GoogleDocParagraphElement = (props: GoogleDocParagraphElementProps) => {
-    
+const BlockTextElement = (props: BlockTextElementProps) => {
     const {
-        element: { content, textStyle }
+        textElement: { content, textStyle },
+        baseSize, gDocBaseSize
     } = props;
 
-    return <>{content}</>
+    const { bold, italic, underline, fontSize } = textStyle;
+    const classes = [];
+    const style: CSSProperties = {};
+
+    if (bold) {
+        classes.push("font-bold");
+    }
+
+    if (italic) {
+        classes.push("italic");
+    }
+
+    if (underline) {
+        classes.push("underline");
+    }
+
+    if (fontSize) {
+        const fsize = baseSize + (fontSize.magnitude - gDocBaseSize);
+        style.fontSize = fsize + 'px';
+    }
+
+    return <span style={style} className={classes.join(" ")}>{content}</span>;
+}
+
+type BlockType = 'Title'|'Heading1'|'Heading2'|'Heading3'|'Paragraph'
+
+type BlockContent = TitleContent|Heading1Content|Heading2Content|Heading3Content|ParagraphContent;
+
+interface BlockBase<T extends BlockType> {
+    readonly elements: TextElement[];
+    readonly blockType: T;
+}
+
+type TitleContent = BlockBase<'Title'>;
+type Heading1Content = BlockBase<'Heading1'>;
+type Heading2Content = BlockBase<'Heading2'>;
+type Heading3Content = BlockBase<'Heading3'>;
+type ParagraphContent = BlockBase<'Paragraph'>;
+
+
+interface TextElement {
+    readonly content: string;
+    readonly textStyle: docs_v1.Schema$TextStyle;
 }
 
 export default GoogleDocDisplayer;
