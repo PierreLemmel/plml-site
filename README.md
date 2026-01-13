9c8f1fab39d4d02f793224a34ccb0e59a11823fb
unknown <pierre.lemmel@gmail.com>
2026-01-13 02:35:32 +0100

    WIP

---
+++ README.md
@@ -0,0 +1,1 @@
+# Plml Website
--- services/animation.ts
+++
@@ -1,9 +0,0 @@
-import { Transition } from "framer-motion";
-
-export function spring(duration: number) : Transition {
-    return { duration, type: 'spring' }
-}
-
-export function linear(duration: number): Transition {
-    return { duration, ease: 'linear' };
-}
