3983e981716a9a6c36a0f313f4328a5f8254ba99
unknown <pierre.lemmel@gmail.com>
2026-01-14 00:25:21 +0100

    Mentions légales

---
+++ src/lib/infos.ts
@@ -0,0 +1,9 @@
+export function getSiteInfos() {
+    return {
+        siteName: 'Plml',
+        companyName: 'Tout est dans le titre',
+        contactEmail: 'pierre.lemmel@gmail.com',
+        instagram: '@p_lemmel',
+        phone: '06 48 03 16 10',
+    };
+}
